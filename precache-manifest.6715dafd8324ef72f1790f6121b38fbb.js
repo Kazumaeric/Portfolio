d2c47015994e93a061f6f632fc3301c3ebf1f523
Kazumaeric <zackkuo2002@gmail.com>
2020-01-14 10:19:19 +0800

    Updates

---
+++ precache-manifest.6715dafd8324ef72f1790f6121b38fbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84968e9cfa26034c21a6c176e30dbc4c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "78ef3c73f4dea6758fa2",
+    "url": "/portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "561a2f1de62eba3fb7e6",
+    "url": "/portfolio/static/css/main.4389017f.chunk.css"
+  },
+  {
+    "revision": "78ef3c73f4dea6758fa2",
+    "url": "/portfolio/static/js/2.87fc397e.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.87fc397e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "561a2f1de62eba3fb7e6",
+    "url": "/portfolio/static/js/main.5d4f0db6.chunk.js"
+  },
+  {
+    "revision": "a4d41ccd057f31d489fb",
+    "url": "/portfolio/static/js/runtime-main.ed8a20de.js"
+  }
+]);
